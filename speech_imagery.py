a23e8477b370f996ee9696e8a41a15264687fea4
chethanponnappaa <chethanponnappa24@gmail.com>
2026-08-06 20:06:38 +0530

    added AI_engine

---
+++ speech_imagery.py
@@ -0,0 +1,18 @@
+import time
+
+def generate_speech_imagery(anchor_word, verbose=True):
+
+    if verbose:
+        print("🧠 Detecting neural speech imagery...")
+        time.sleep(0.3)
+
+        print("📡 Decoding imagined phonemes...")
+        time.sleep(0.3)
+
+        print("🔤 Extracting anchor word...")
+        time.sleep(0.3)
+
+        print("🤖 Constructing sentence...")
+        time.sleep(0.3)
+
+    return anchor_word
